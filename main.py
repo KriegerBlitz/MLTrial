3543d1758e876475690e3cdf670319b8e7c019cc
Saksham <saksham.sg007@gmail.com>
2026-06-18 23:56:14 +0400

    Final

--- main.py
+++ main.py
@@ -4,68 +4,113 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
+import time
+from rich.console import Console
+from rich.table import Table
 
 data = pd.read_csv('salary_prediction_data.csv')
-data = pd.get_dummies(data, drop_first=True) #Turns text data into one-hot binary data since robots can't read
+data = pd.get_dummies(data, drop_first=True) #Turns text data into one-hot binary data
 
 y = data['Salary']
 X = data.drop('Salary', axis=1)
 
-scaler = StandardScaler()
+scaler_X = StandardScaler()
+scaler_Y = StandardScaler()
 
-r2Linear = 0
-r2RF = 0
-r2SVR = 0
-r2DT = 0
-
-mseLinear = 0
-mseRF = 0
-mseSVR = 0
-mseDT = 0
-
+AvgTrainingTime = {
+        "Linear Regressor" : 0,
+        "Random Forest Regressor" : 0,
+        "Decision Tree Regressor" : 0,
+        "Support Vector Regressor" : 0,
+        "MLP Regressor" : 0
+}
+AvgPredictionTime = {
+        "Linear Regressor" : 0,
+        "Random Forest Regressor" : 0,
+        "Decision Tree Regressor" : 0,
+        "Support Vector Regressor" : 0,
+        "MLP Regressor" : 0
+}
+AvgError = {
+    "Linear Regressor" : [0,0],
+    "Random Forest Regressor" : [0,0],
+    "Decision Tree Regressor" : [0,0],
+    "Support Vector Regressor" : [0,0],
+    "MLP Regressor" : [0,0]
+}
 for i in range(10): # Test multiple times and take the average to eliminate luck.
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=i) #I think this is called a monty carlo?
-    Xtrain = scaler.fit_transform(X_train)
-    Xtest = scaler.transform(X_test)
+    Xtrain = scaler_X.fit_transform(X_train)
+    Xtest = scaler_X.transform(X_test)
+    ytrain = scaler_Y.fit_transform(y_train.values.reshape(-1,1)).ravel()
+    ytest = scaler_Y.transform(y_test.values.reshape(-1,1)).ravel()
 
-    LinearRegressor = LinearRegression()
-    RFRegressor = RandomForestRegressor(random_state=i) #not sure if the parameters have to be named or if it's just for clarity
-    DTRegressor = DecisionTreeRegressor(random_state=i)
-    SVRRegressor = SVR(C=10000) #salary numbers tend to be big so we need a larger penalty
+    Models = {
+        "Linear Regressor" : LinearRegression(),
+        "Random Forest Regressor" : RandomForestRegressor(random_state=i),
+        "Decision Tree Regressor" : DecisionTreeRegressor(random_state=i),
+        "Support Vector Regressor" : SVR(),
+        "MLP Regressor" : MLPRegressor(max_iter=500)
+    }
+    Predictions = {
+        "Linear Regressor": None,
+        "Random Forest Regressor": None,
+        "Decision Tree Regressor": None,
+        "Support Vector Regressor": None,
+        "MLP Regressor": None
+    }
 
-    LinearRegressor.fit(Xtrain, y_train)
-    RFRegressor.fit(Xtrain, y_train)
-    DTRegressor.fit(Xtrain, y_train)
-    SVRRegressor.fit(Xtrain, y_train) #Autocomplete on pycharm is incredible, I didn't have to type 90% of this
+    for name, model in Models.items():
+        start = time.perf_counter()
+        model.fit(Xtrain, ytrain)
+        end = time.perf_counter()
+        AvgTrainingTime[name] += (end - start)/10
 
-    pLinear = LinearRegressor.predict(Xtest)
-    pRF = RFRegressor.predict(Xtest)
-    pDT = DTRegressor.predict(Xtest)
-    pSVR = SVRRegressor.predict(Xtest)
+        start = time.perf_counter()
+        Predictions[name] = model.predict(Xtest)
+        end = time.perf_counter()
+        AvgPredictionTime[name] += (end - start)/10
 
-    r2Linear += r2_score(y_test, pLinear)
-    r2RF += r2_score(y_test, pRF)
-    r2SVR += r2_score(y_test, pSVR)
-    r2DT += r2_score(y_test, pDT)
+        AvgError[name][0] += mean_squared_error(ytest, Predictions[name])/10
+        AvgError[name][1] += r2_score(ytest, Predictions[name])/10
 
-    mseLinear += mean_squared_error(y_test, pLinear)
-    mseRF += mean_squared_error(y_test, pRF)
-    mseSVR += mean_squared_error(y_test, pSVR)
-    mseDT += mean_squared_error(y_test, pDT)
+console = Console()
+
+table = Table(title="Results", show_header=True, header_style="bold magenta")
+
+table.add_column("Algorithm", style="cyan", justify="left")
+table.add_column("Training Time", justify="right")
+table.add_column("Prediction Time", justify="right")
+table.add_column("MSE", style="bold red",justify="right")
+table.add_column("R-Squared", style="bold green", justify="right")
+
+for name in Models.keys():
+    table.add_row(
+        name,
+        f"{AvgTrainingTime[name]:.4f}s",
+        f"{AvgPredictionTime[name]:.4f}s",
+        f"{AvgError[name][0]:.4f}",
+        f"{AvgError[name][1]:.4f}"
+    )
+
+console.print()
+console.print(table)
 
 
-print("Linear model amse: ", mseLinear/10, " ar2: ",r2Linear/10)
-print("RF  model amse: ", mseRF/10, " ar2: ",r2RF/10)
-print("SVR model amse: ", mseSVR/10, " ar2: ",r2SVR/10)
-print("DT model amse: ", mseDT/10, " ar2: ",r2DT/10)
-
-# Results from test:
-# Linear model amse:  99674975.13450691  ar2:  0.8728807244612167
-# RF  model amse:  131496990.6114753  ar2:  0.8318260546360072
-# SVR model amse:  115758747.41165285  ar2:  0.8522392071029163
-# DT model amse:  231679145.43609816  ar2:  0.703120654542128
-# For this data, it seems the simple linear model outperforms others consistently. This makes me think the data is
-# probably forged. This is how the Schoen scandal came out, after all. The second derivative would be close to 0
+#Results on my computer:
+# ┌──────────────────────┬───────────────┬─────────────────┬────────┬───────────┐
+# │ Algorithm            │ Training Time │ Prediction Time │    MSE │ R-Squared │
+# ├──────────────────────┼───────────────┼─────────────────┼────────┼───────────┤
+# │ Linear Regressor     │       0.0005s │         0.0001s │ 0.1244 │    0.8729 │
+# │ Random Forest        │       0.1311s │         0.0063s │ 0.1640 │    0.8317 │
+# │ Regressor            │               │                 │        │           │
+# │ Decision Tree        │       0.0017s │         0.0002s │ 0.2929 │    0.6987 │
+# │ Regressor            │               │                 │        │           │
+# │ Support Vector       │       0.0147s │         0.0096s │ 0.1492 │    0.8473 │
+# │ Regressor            │               │                 │        │           │
+# │ MLP Regressor        │       0.2537s │         0.0002s │ 0.1417 │    0.8549 │
+# └──────────────────────┴───────────────┴─────────────────┴────────┴───────────┘
